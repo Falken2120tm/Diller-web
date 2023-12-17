f9dc9fa51c2c84435f693bb5aca189c437d4bf4c
Falken2120tm <falken2120tm@gmail.com>
2023-12-18 00:45:03 +0300

    Old project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+for(;;){
+
+  for(;;)
+
+}
+
+a;
+b;
+
+while(a<=5){
+  b=+a
+}
